9484b45c82e199ca8f0e624dd22ec829f8219dfa
Jonas <Jonas@Jonas-PC.fritz.box>
2018-03-08 19:33:14 +0100

    GameClient base

--- shipripper/src/shipripper/GameClient.java
+++ shipripper/src/shipripper/GameClient.java
@@ -4,24 +4,41 @@ import java.util.Scanner;
 
 public class GameClient extends util.Client{
 	
-	private String name;
+	private Player player;
+	private int state;
+	private Scanner scanner;
 
 	public GameClient(String serverIP, int serverPort, String pName) {
 		super(serverIP, serverPort);
-		name = pName;
+		player = new Player(pName);
+		state = Game.STATE_WAITING;
+		if(!isConnected()) {
+			System.out.println("Kein Server unter " + serverIP + ":" + serverPort + " zu erreichen.");
+		}
+		System.out.println("An Server " + serverIP + ":" + serverPort + " anmelden...");
+		send("READY " + pName);
+		scanner = new Scanner(System.in);
 	}
 	
-	/*public GameClient() {
-		System.out.println("Server IP:");
-		Scanner scanner = new Scanner(System.in);
-		String serverIP = scanner.nextLine();
-		System.out.println("Server Port:");
-		String serverPortS = scanner.nextLine();
-		int serverPort = Integer.parseInt(serverPortS);
-		
-		super(serverIP, serverPort);
-	}*/
-
+	private void gameLoop() {
+		while(true){
+			String input = scanner.nextLine();
+			String[] in = input.split(" ");
+			
+			if(in[0].equalsIgnoreCase("GEGNER")) {
+				//Gegnerfeld ausgeben
+			}else if(in[0].equalsIgnoreCase("EIGEN")) {
+				//Eigenes Feld ausgeben
+			}else if(in[0].equalsIgnoreCase("PLACE")) {
+				//Schiff platzieren
+			}else if(in[0].equalsIgnoreCase("SHOOT")) {
+				//Schieﬂen
+			}else if(in[0].equalsIgnoreCase("QUIT")) {
+				//Spiel beenden
+			}
+		}
+	}
+	
 	@Override
 	public void processMessage(String pMessage) {
 		// TODO Auto-generated method stub
